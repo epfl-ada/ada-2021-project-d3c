5c33767744380864398e9a5a2a4f1c5cba818bdb
Shiny-Su <suxiaotian31@gmail.com>
2022-05-01 09:54:29 +0200

    Add files

--- plotly_apps/domain-dash/app.py
+++
@@ -1,6 +0,0 @@
-import dash
-
-#Instantiates the Dash app and identify the server
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
